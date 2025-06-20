c94dd67d94343b8891225ef03443fc345207fd84
Peteware <petewarescripts@gmail.com>
2025-06-21 00:59:22 +0100

    Update 126884695634066.lua

--- Games/126884695634066.lua
+++ Games/126884695634066.lua
@@ -1,4 +1,22 @@
-_G.Initialised = true
+if keyUI then
+    for _, obj in ipairs(keyUI:GetDescendants()) do
+        if obj:IsA("TextLabel") or obj:IsA("TextButton") or obj:IsA("TextBox") then
+            tweenService:Create(obj, TweenInfo.new(0.4), {
+                TextTransparency = 1
+            }):Play()
+        elseif obj:IsA("Frame") then
+            tweenService:Create(obj, TweenInfo.new(0.4), {
+                BackgroundTransparency = 1
+            }):Play()
+        elseif obj:IsA("ImageLabel") or obj:IsA("ImageButton") then
+            tweenService:Create(obj, TweenInfo.new(0.4), {
+                ImageTransparency = 1
+            }):Play()
+        end
+    end
+
+    task.delay(0.45, function()
+        keyUI:Destroy()
+    end)
+end
 loadstring(game:HttpGet('https://raw.githubusercontent.com/skxller1/Test/refs/heads/main/Growagarden'))()
-task.wait(2.5)
-_G.Initialised = nil
